cc185bc60966559bcf195bc8d3deda34023c7296
Isaac Buitrago <mightyking7@gmail.com>
2020-04-08 22:57:38 -0500

    Cleaned up visualization code

--- model.py
+++ model.py
@@ -8,27 +8,42 @@ if sys.platform == "darwin":
 
 import mplfinance as mpf
 
-"""
-load coin data into dataframe
-"""
-def loadData(coin:str) -> pd.DataFrame:
-    root = "./data"
 
-    csvFile = os.path.join(root, coin + "_price.csv")
+class CoinGraph:
 
-    with open(csvFile) as csv:
-        df = pd.read_csv(csv, index_col=0, parse_dates=True)
+    def __init__(self, coin:str):
 
-    return df
+        self.coin = coin.lower() # coin name
+        self.root = "./data"
+        self.df = None # data frame of prices
 
-"""
-"""
-def render(df:pd.DataFrame):
+    """
+    load coin data into dataframe
+    """
+    def loadCoinData(self) -> pd.DataFrame:
 
-    kwargs = dict(type='candle', style="charles")
-    mpf.plot(df, **kwargs)
+        # initial csv read
+        if type(self.df) != pd.DataFrame:
+
+            csvFile = os.path.join(self.root, self.coin + "_price.csv")
+
+            with open(csvFile) as csv:
+                self.df = pd.read_csv(csv, index_col=0, parse_dates=True)
+
+        return self.df
+
+    """
+    """
+    def render(self):
+
+        if type(self.df) != pd.DataFrame:
+            print(f"Error: Must load data for {self.coin} before usage")
+            return
+
+        mpf.plot(self.df, type="candle", style="charles",
+                          title=self.coin.title(), ylabel_lower="Volume")
 
 
-df_bitcoin = loadData("qtum")
-render(df_bitcoin)
-
+cg = CoinGraph("Bitcoin")
+df_bitcoin = cg.loadCoinData()
+cg.render()
