45a51ea248d8b6ea5e6463e1d07ff832afa1f14a
Isaac Buitrago <mightyking7@gmail.com>
2020-04-22 17:26:25 -0500

    Initial LSTM to predict bitcoin prices

--- isaac_model.py
+++ isaac_model.py
@@ -3,7 +3,6 @@ import sys
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-
 from keras.models import Sequential, load_model
 from keras.layers import Dense
 from keras.layers import LSTM
@@ -14,6 +13,7 @@ min_max_scaler = MinMaxScaler()
 # directories for model and saved images
 imagesDir = "./images/"
 modelDir = "./model/"
+fname = "lstm.h5"
 
 # make images dir for plots if non exists
 if not os.path.exists(imagesDir):
@@ -23,11 +23,21 @@ if not os.path.exists(imagesDir):
 if not os.path.exists(modelDir):
     os.mkdir(modelDir)
 
-# read data, drop date col, transform  volume and market cap to int
+# read data
 df = pd.read_csv("./data/bitcoin_price.csv")
-df.drop(["Date", "Volume", "Market Cap"], axis=1, inplace=True)
-# s_vol = df["Volume"].apply(lambda x: x.replace(',', ''))
-# s_vol = s_vol.apply(lambda x: float(x))
+
+# drop date col
+df.drop(["Date"], axis=1, inplace=True)
+
+# replace '-' with np.NaN
+df.replace('-', np.NaN, inplace=True)
+
+# drop rows with NaN
+df.dropna(axis=0, how="any", inplace=True)
+
+#  transform volume and market cap into floats
+df["Volume"] = df["Volume"].str.replace(',', '').astype('float64')
+df["Market Cap"] = df["Market Cap"].str.replace(',', '').astype('float64')
 
 # split into training and testing
 prediction_days = 30
@@ -41,42 +51,44 @@ training_set = min_max_scaler.fit_transform(training_set)
 
 x_train = training_set[0:len(training_set)-1]
 y_train = training_set[1:len(training_set)+1]
-x_train = np.reshape(x_train, (len(x_train), 1, 4))
-print(x_train.shape)
-print(y_train.shape)
+x_train = x_train.reshape((len(x_train), 1, 6))
 
-num_units = 4
+num_units = 6
 activation_function = 'sigmoid'
 optimizer = 'adam'
 loss_function = 'mean_squared_error'
 batch_size = 5
 num_epochs = 100
 
+# load model if it exits
+if os.path.exists(modelDir + fname):
+    regressor = load_model(modelDir + fname)
 
-# Initialize the RNN
-regressor = Sequential()
+else:
+    # Initialize the RNN
+    regressor = Sequential()
 
-# Adding the input layer and the LSTM layer
-regressor.add(LSTM(units = num_units, activation = activation_function, input_shape=(1, 4)))
+    # Adding the input layer and the LSTM layer
+    regressor.add(LSTM(units = num_units, activation = activation_function, input_shape=(1, 6)))
 
-# Adding the output layer
-regressor.add(Dense(units = 4))
+    # Adding the output layer
+    regressor.add(Dense(units = 6))
 
-# Compiling the RNN
-regressor.compile(optimizer = optimizer, loss = loss_function)
+    # Compiling the RNN
+    regressor.compile(optimizer = optimizer, loss = loss_function)
 
-# Using the training set to train the model
-regressor.fit(x_train, y_train, batch_size = batch_size, epochs = num_epochs)
+    # Using the training set to train the model
+    regressor.fit(x_train, y_train, batch_size = batch_size, epochs = num_epochs)
 
-# save model
-regressor.save(modelDir + "lstm.h5")
+    # save model
+    regressor.save(modelDir + fname)
 
 # predict price
 test_set = df_test.values
 
-inputs = np.reshape(test_set, (len(test_set), 1))
+inputs = np.reshape(test_set, (len(test_set), 6))
 inputs = min_max_scaler.transform(inputs)
-inputs = np.reshape(inputs, (len(inputs), 1, 1))
+inputs = np.reshape(inputs, (len(inputs), 1, 6))
 
 predicted_price = regressor.predict(inputs)
 predicted_price = min_max_scaler.inverse_transform(predicted_price)
