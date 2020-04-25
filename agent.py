e7005e324590290603bb64b7123386816a2659db
jbshiau235 <jskbmksh235@gmail.com>
2020-04-25 18:58:41 -0500

    Added tie breaker decision maker
    Based on distance closest to mean

--- agent.py
+++ agent.py
@@ -52,10 +52,12 @@ isaac_input.loc[0] = np.empty(6)
 for coin in list(trent_output.columns):
     if len(trent_output[coin].mode()) < 10:
         modes = list(trent_output[coin].mode())
+        mean = trent_output[coin].mean()
         d = defaultdict(float)
         for mode in modes:
-            d[mode] += 1
-        result = max(d.items(), key=lambda x: x[1])
-        isaac_input[coin] = result[0]
+            distance = abs(mode - mean)
+            d[mode] = distance
+        result = min(d.items(), key=lambda x: x[1])
+        isaac_input[coin] = trent_output[coin].mode()[0]
     else:
         isaac_input[coin] = trent_output[coin].median()
